6d39fdf09c1bb8e9dcc8f56eb79c628e1d030118
Roan-1076060 <1076060@hr.nl>
2023-09-29 22:14:30 +0200

    update 1.1

--- main.js
+++ main.js
@@ -1,40 +1,48 @@
-const { app, BrowserWindow, Notification } = require('electron')
-const { ipcMain } = require('electron');
-const path = require('path');
+const { app, BrowserWindow, Notification } = require("electron");
+const { ipcMain } = require("electron");
+const path = require("path");
 
 app.whenReady().then(() => {
-    // Makes the window
-    const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        icon: path.join(__dirname, './public/app.png'),
-        webPreferences: {
-            nodeIntegration: false, // is default value after Electron v5
-            contextIsolation: true, // protect against prototype pollution
-            enableRemoteModule: false, // turn off remote
-        }
-    })
+  // Makes the window
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    autoHideMenuBar: true,
+    icon: path.join(__dirname, "./public/appstore.png"),
+    webPreferences: {
+      nodeIntegration: false, // is default value after Electron v5
+      contextIsolation: true, // protect against prototype pollution
+      enableRemoteModule: false, // turn off remote
+    },
+  });
 
-    // Checks if platform is windows, if it is, gives the app a name.
-    if (process.platform === 'win32') {
-        app.setAppUserModelId("Skiff Electron");
+  // Checks if platform is windows, if it is, gives the app a name.
+  if (process.platform === "win32") {
+    app.setAppUserModelId("Skiff Electron");
+  }
+
+  // Loads the URL
+  mainWindow.loadURL("https://app.skiff.com/mail");
+
+  // Add event listener to the console object
+  mainWindow.webContents.on(
+    "console-message",
+    (event, level, message, line, sourceId) => {
+      if (message.includes("emails, requerying")) {
+        // Executes function on recieving Mail
+        MailNotifier();
+      }
     }
+  );
 
-    // Loads the URL
-    mainWindow.loadURL('https://app.skiff.com/mail')
-
-    // Add event listener to the console object
-    mainWindow.webContents.on('console-message', (event, level, message, line, sourceId) => {
-        if (message.includes('emails, requerying')) {
-            // Executes function on recieving Mail
-            MailNotifier();
-        }
-    });
-
-    function MailNotifier() {
-        // New email notification
-        const NOTIFICATION_TITLE = 'Skiff Mail'
-        const NOTIFICATION_BODY = 'You recieved a new mail'
-        new Notification({ title: NOTIFICATION_TITLE, body: NOTIFICATION_BODY, icon: './public/app.png' }).show()
-    }
+  function MailNotifier() {
+    // New email notification
+    const NOTIFICATION_TITLE = "Skiff Mail";
+    const NOTIFICATION_BODY = "You recieved a new mail";
+    new Notification({
+      title: NOTIFICATION_TITLE,
+      body: NOTIFICATION_BODY,
+      icon: "./public/app.png",
+    }).show();
+  }
 });
